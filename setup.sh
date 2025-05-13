a00b444ec9048a4d29e40524e2548f314a398844
Daniel Lundgaard <202004134@post.au.dk>
2025-05-13 09:51:28 +0200

    submission

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 python -m venv venv
 source venv/bin/activate
 pip install -U -r requirements.txt
-pip install ipykernel jupyter ipywidgets nbformat
-python -m ipykernel install --user --name="kernel"
 
 KALEIDO_FAULTY_SCRIPT_PATH=venv/lib/python3.12/site-packages/kaleido/executable/kaleido
 sed -i 's/cd $DIR/cd "$DIR"/g' $KALEIDO_FAULTY_SCRIPT_PATH
